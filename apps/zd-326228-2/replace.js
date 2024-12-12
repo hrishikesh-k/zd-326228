2805f2d6667340a6c1ec32a6f471186548700356
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-12-13 01:42:25 +0800

    try standalone

--- apps/zd-326228-2/replace.js
+++ apps/zd-326228-2/replace.js
@@ -2,7 +2,7 @@ import { cwd } from 'node:process'
 import { join } from 'node:path'
 import { readFileSync, writeFileSync } from 'node:fs'
 
-const prerenderManifestJsonPath = join(cwd(), '.next', 'prerender-manifest.json')
+const prerenderManifestJsonPath = join(cwd(), '.next', 'standalone', '.next', 'prerender-manifest.json')
 const prerenderManifestJsonObject = JSON.parse(readFileSync(prerenderManifestJsonPath, 'utf-8'))
 
 prerenderManifestJsonObject.preview.previewModeId = '12345678901234567890'
